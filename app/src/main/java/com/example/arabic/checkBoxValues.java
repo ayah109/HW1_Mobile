e95f405f6dfd5673f78b3dff5a66acd9cfbf6e20
Aya Hussain <ayatalep816@gmail.com>
2023-05-05 22:36:46 +0300

    version 1-not complete

---
+++ app/src/main/java/com/example/arabic/checkBoxValues.java
@@ -0,0 +1,43 @@
+package com.example.arabic;
+
+public class checkBoxValues {
+    public static final String[] BUTTON_A_VALUES = {"جوهرة", " لوح", "طاولة ","مجد", "برج", "خوخ"};
+    public static final String[] BUTTON_B_VALUES = {"مسرح", "ثائر", "مثال","كتاب", "وطن", "ثلث"};
+    public static final String[] BUTTON_C_VALUES = {"باب", "تمر", "خزانة","أكل", "متر", "تهاني"};
+    public static final String[] BUTTON_D_VALUES = {"شمس", "برتقال", "مشروع","شراب", "قلب", "دمية"};
+    public static final String[] BUTTON_E_VALUES = {"ألوان", "عملية", "عصير","أمجد", "مأزق", "عقرب"};
+    public static final String[] BUTTON_F_VALUES = {"حيوان", "مريض", "طاقم","رمح", "سحابة", "بحر"};
+    public static final String[] BUTTON_G_VALUES = {"ربيع", "دائرة", "ذرة","كابتن", "رأس", "منفضة"};
+    public static final String[] BUTTON_H_VALUES = {"مديد", "صفر", "دائرة","صوت", "زهرة", "مقعد"};
+    public static final String[] BUTTON_I_VALUES = {"خوخ", "خروف", "حبل","طائر", "داخل", "مساء"};
+    public static final String[] BUTTON_J_VALUES = {"حمل", "محلول", "فحم","راية", "فرح", "ثمانية"};
+    public static final String[] BUTTON_K_VALUES = {"طاقة", "بلد", "ضابط","قارب", "ضرس", "ثور"};
+    public static final String[] BUTTON_L_VALUES = {"حكيم", "صديق", "صلاة","صرصار", "شمس", "صدقة"};
+    public static final String[] BUTTON_M_VALUES = {"شرشبيل", "حمار", "شرس","زوجة", "بشارة", "رقم"};
+    public static final String[] BUTTON_N_VALUES = {"مستقبل", "مدرب", "سلسبيل","خبز", "دعوة", "عنوان"};
+    public static final String[] BUTTON_O_VALUES = {"زينب", "طبيب", "نجم","جزيرة", "ماعز", "معلم"};
+    public static final String[] BUTTON_P_VALUES = {"حقيبة", "صورة", "فقمة","مكتب", "فريق", "ملفوف"};
+    public static final String[] BUTTON_Q_VALUES = {"ملكة", "إبحار", "فراب", "ريشة", "مغرب", "غيمة"};
+    public static final String[] BUTTON_R_VALUES = {"عدسة", "مجرة", "معيد", "طائر", "عسل", "حرية"};
+    public static final String[] BUTTON_S_VALUES = {"ظفر", "تراب", "حديقة", "ظهر", "ساعة", "بوظة"};
+    public static final String[] BUTTON_T_VALUES = {"طالب", "زيتون", "طبيب", "فراولة", "بطوط", "موز"};
+    public static final String[] BUTTON_U_VALUES = {"مطر", "نسر", "رعد", "سن", "ضباب", "سنجاب"};
+    public static final String[] BUTTON_V_VALUES = {"مكتب", "كمبيوتر", "طباعة", "سماعات", "كاميرا", "شاشة"};
+    public static final String[] BUTTON_W_VALUES = {"شاطئ", "موجة", "رمل", "لامة", "شمس", "سلم"};
+    public static final String[] BUTTON_X_VALUES = {"شباك", "سجادة", "نافذة", "شمعة", "كرسي", " كتب"};
+    public static final String[] BUTTON_Y_VALUES = {"مشط", "قطة", "منشفة", "مرآة", "قرنبيط", "صابون"};
+    public static final String[] BUTTON_Z_VALUES = {"زهرة", "مهر", "عشب", "صخرة", "هارب", "أوراق"};
+    public static final String[] BUTTON_waw_VALUES = {"وردة", "عطر", "سوار", "مسجد", "فناء", "رؤوس"};
+    public static final String[] BUTTON_ya_VALUES = {"قطار", "شريحة", "مستطيل","عود", "خضرة", "ياسر"};
+
+
+
+
+
+
+
+
+
+
+
+}
